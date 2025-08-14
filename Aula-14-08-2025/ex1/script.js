70263345bd3fd3387c54a93c1632599da6d18167
Ranyelson Neres <ranyelson.carvalho@p.ucb.br>
2025-08-14 10:14:07 -0300

    Create script.js

---
+++ Aula-14-08-2025/ex1/script.js
@@ -0,0 +1,19 @@
+// let y=parseInt(window.prompt("ate onde vai a soma"));
+// let sum=0;
+// let counter='1';
+
+// for(let i=0;i<y;i++){
+//     sum+=parseInt(counter);
+//     counter+='1';
+// }
+
+// console.log(sum);
+
+let i;
+const termo = 8;
+let resultado = "";
+
+for(i = 1; i <= termo; i++) {
+    resultado = `${resultado}${i}`
+    document.writeln(`${resultado} <br>`)
+}
